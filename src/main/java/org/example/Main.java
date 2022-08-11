cefb15c73ab670e4fa606432579b36a84d6f1862
CHARPG <56536969+CHARPG@users.noreply.github.com>
2022-08-11 12:05:13 +0100

    reverted logger becuase I dont understand it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,6 @@
 package org.example;
 import java.util.ArrayList;
-import java.util.logging.Logger;
 public class Main {
-    private static Logger LOGGER = Logger.getLogger("InfoLogging");
     protected static final ArrayList<Cat> catArray = new ArrayList<>();
     protected static final ArrayList<FlyingRat> flyingRatArray = new ArrayList<>();
     private static String[] birdNames = {"Blungo", "Cungo", "Dungo"};
@@ -18,29 +16,29 @@ public class Main {
         }
 
         //breeding
-        LOGGER.info("Amount of cats: " + catArray.size());
+        System.out.println("Amount of cats: " + catArray.size());
 
-        LOGGER.info(catArray.get(0).breed(catArray.get(0), catArray.get(1)));
+        System.out.println(catArray.get(0).breed(catArray.get(0), catArray.get(1)));
         catArray.get(2).setName(catNames[2]);
         catArray.get(2).setAge(0);
 
-        LOGGER.info(catArray.get(2).getName() + " now exists");
+        System.out.println(catArray.get(2).getName() + " now exists");
 
-        LOGGER.info("Amount of cats: " + catArray.size() + "\n");
+        System.out.println("Amount of cats: " + catArray.size() + "\n");
 
         //cat actions
-        LOGGER.info(catArray.get(0).eat() + "\n");
+        System.out.println(catArray.get(0).eat() + "\n");
 
         //bird actions
-        LOGGER.info(flyingRatArray.get(0).eat());
+        System.out.println(flyingRatArray.get(0).eat());
 
         //cat killing
         for(int i = 0; i < flyingRatArray.size(); i++)
         {
-            LOGGER.info(catArray.get(i).move(flyingRatArray.get(i).getName()));
+            System.out.println(catArray.get(i).move(flyingRatArray.get(i).getName()));
 
-            LOGGER.info(catArray.get(i).kill(flyingRatArray.get(i--)) + "\n");
+            System.out.println(catArray.get(i).kill(flyingRatArray.get(i--)) + "\n");
         }
-        LOGGER.info("Amount of flying rats " + flyingRatArray.size());
+        System.out.println("Amount of flying rats " + flyingRatArray.size());
     }
 }
